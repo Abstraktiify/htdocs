f65b33614f60989cd9157963d12abb0e8a358aef
Abstraktiify <oliver.lundin.te16d@gmail.com>
2018-01-18 18:08:44 +0100

    Uppgift 1

---
+++ javascript/index.php
@@ -0,0 +1,19 @@
+<!doctype html>
+<html>
+
+    <head>
+        <meta charset="utf-8">
+        <link rel="stylesheet" type="text/css" href="style.css">
+    </head>
+    
+    <body>
+        <button id="alertBtn" type="button" onclick="alertClick()">Alert</button>
+        
+        <script>
+        function alertClick() {
+            alert("ayyyyLMAO");
+        }
+        </script>
+    </body>
+    
+</html>
