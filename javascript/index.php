05e6cd64f581c345e8d9d816dfda9f7623f44ed4
Abstraktiify <oliver.lundin.te16d@gmail.com>
2018-01-19 10:49:18 +0100

    Uppgift 3
    God afton

--- javascript/index.php
+++ javascript/index.php
@@ -12,24 +12,39 @@
         
         <script>
             
-        function alertClick() {
-            alert("ayyyyLMAO");
-        }
+            function alertClick() {
+                alert("ayyyyLMAO");
+            }
             
-        function picHover(x) {
-            x.style.height = "128px";
-            x.style.width = "128px";
-        }
+            function picHover(x) {
+                x.style.height = "128px";
+                x.style.width = "128px";
+            }
             
-        function picNoHover(x) {
-            x.style.height = "64px";
-            x.style.width = "64px";
-        }
+            function picNoHover(x) {
+                x.style.height = "64px";
+                x.style.width = "64px";
+            }
+            
+            function hideShowImg() {
+                var x = document.getElementById("img");
+                if (x.style.display === "none") {
+                    x.style.display = "block";
+                } else {
+                    x.style.display = "none";
+                }
+            }
             
         </script>
         
-        <div id="eksde">
+        <div id="img">
             <img onmouseover="picHover(this)" onmouseout="picNoHover(this)" src="084.png">
+        </div>
+        
+        <div id="hideShowImg">
+            <div id="hideShowImgBtn">
+                <button id="hideImgButton" type="button" onclick="hideShowImg()">Toggle Picture</button>
+            </div>
         </div>
         
     </body>
