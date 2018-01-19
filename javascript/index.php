28128968ff4a17a748e3edac66259b1b61bdc063
Abstraktiify <oliver.lundin.te16d@gmail.com>
2018-01-19 10:29:23 +0100

    Uppgift 2
    gokväll

--- javascript/index.php
+++ javascript/index.php
@@ -7,13 +7,31 @@
     </head>
     
     <body>
+        
         <button id="alertBtn" type="button" onclick="alertClick()">Alert</button>
         
         <script>
+            
         function alertClick() {
             alert("ayyyyLMAO");
         }
+            
+        function picHover(x) {
+            x.style.height = "128px";
+            x.style.width = "128px";
+        }
+            
+        function picNoHover(x) {
+            x.style.height = "64px";
+            x.style.width = "64px";
+        }
+            
         </script>
+        
+        <div id="eksde">
+            <img onmouseover="picHover(this)" onmouseout="picNoHover(this)" src="084.png">
+        </div>
+        
     </body>
     
 </html>
