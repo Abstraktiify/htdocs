e3e9a020d1686e809118f2b4833fb8908b578678
Abstraktiify <oliver.lundin.te16d@gmail.com>
2018-01-20 15:52:33 +0100

    Uppgift 5 & 6
    Missade den ensamma commiten på Uppgift 5 och 6, så det får bli en dubbel. Fixade även border samt text som visar vilka knappar/saker är vilken uppgift.

--- javascript/index.php
+++ javascript/index.php
@@ -8,8 +8,6 @@
     
     <body>
         
-        <button id="alertBtn" type="button" onclick="alertClick()">Alert</button>
-        
         <script>
             
             function alertClick() {
@@ -36,25 +34,99 @@
             }
             
             function changeBodyColor() {
-                document.body.style.backgroundColor = "red";
+                r = Math.floor(Math.random()*255);
+                g = Math.floor(Math.random()*255);
+                b = Math.floor(Math.random()*255);
+                document.body.style.backgroundColor =       "rgb("+r+","+g+","+b+")";
+            }
+            
+            function clearBody() {
+                
+                document.body.innerHTML="";
+                
+                var h1 = document.createElement("h1");
+                var t = document.createTextNode("Hecking site just got cleared boii");
+                h1.appendChild(t);
+                document.body.appendChild(h1);
+
+                }
+            
+            function greenBorderBG() {
+                var x = document.getElementById("actualBorder");
+                
+                x.style.backgroundColor = "green";
+                
+            }
+            
+            function redBorderBG() {
+                var x = document.getElementById("actualBorder");
+                
+                x.style.backgroundColor = "red";
+                
+            }
+            
+            function blueBorderBG() {
+                var x = document.getElementById("actualBorder");
+                
+                x.style.backgroundColor = "blue";
+                
+            }
+            
+            function resetBorderBG() {
+                var x = document.getElementById("actualBorder");
+                
+                x.style.backgroundColor = "white";
             }
             
         </script>
         
+        <div id="main">
+        
+            <p>Uppgift 1</p>
+            
+        <button id="alertBtn" type="button" onclick="alertClick()">Alert</button>
+        
         <div id="img">
+            <p>Uppgift 2</p>
             <img onmouseover="picHover(this)" onmouseout="picNoHover(this)" src="084.png">
         </div>
         
         <div id="hideShowImg">
+            <p>Uppgift 3</p>
             <div id="hideShowImgBtn">
                 <button id="hideImgButton" type="button" onclick="hideShowImg()">Toggle Picture</button>
             </div>
         </div>
         
         <div id="changeBGColor">
+            <p>Uppgift 4</p>
             <button id="bgColorBtn" onclick="changeBodyColor()">Change Color</button>
         </div>
         
+            </div>
+
+        
+        <div id="clearBtn">
+            <p>Uppgift 5</p>
+            <button onclick="clearBody()" id="clearButton">Clear Site</button>
+        </div>
+        
+        <div id="borderBGChange">
+            
+            <button onclick="greenBorderBG()" id="green">Green</button>
+            
+            <button onclick="blueBorderBG()" id="red">Red</button>
+            
+            <button onclick="redBorderBG()" id="blue">Blue</button>
+            
+            <button onclick="resetBorderBG()" id="reset">Reset</button>
+            
+            <div id="actualBorder">
+                
+            </div>
+        
+        </div>
+            
     </body>
     
 </html>
